b8426ca659ccdb5674009dc077e0445266d2ad33
Fernando Naufel <fnaufel@gmail.com>
2021-10-29 18:14:28 -0300

    Solve LaTex problems (lexend, titlesec)

--- _build.sh
+++ _build.sh
@@ -4,4 +4,4 @@ set -ev
 
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::bs4_book')"
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
-
+mv -f docs/_main.pdf docs/probestr.pdf
