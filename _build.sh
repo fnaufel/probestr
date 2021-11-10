75dd984560a3cbc9ecc6b75ddd1ed3843508e8f1
Fernando Naufel <fnaufel@gmail.com>
2021-11-10 18:10:56 -0300

    Finish main body of ch 3

--- _build.sh
+++ _build.sh
@@ -2,6 +2,11 @@
 
 set -ev
 
+rm /home/fnaufel/Documents/UFF/Ensino/Disciplinas/Probabilidade-e-estatistica/Bookdown/docs/dfimg/*
+
+rm /home/fnaufel/Documents/UFF/Ensino/Disciplinas/Probabilidade-e-estatistica/Bookdown/dfimg/*
+
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::bs4_book')"
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
+
 mv -f docs/_main.pdf docs/probestr.pdf
