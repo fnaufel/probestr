c2da5cf7fad447d4bedd76afbf741667ec3942c4
Fernando Naufel <fnaufel@gmail.com>
2021-11-11 15:48:31 -0300

    Finish exercises ch viz

--- _build.sh
+++ _build.sh
@@ -2,9 +2,9 @@
 
 set -ev
 
-rm /home/fnaufel/Documents/UFF/Ensino/Disciplinas/Probabilidade-e-estatistica/Bookdown/docs/dfimg/*
+rm -f /home/fnaufel/Documents/UFF/Ensino/Disciplinas/Probabilidade-e-estatistica/Bookdown/docs/dfimg/*
 
-rm /home/fnaufel/Documents/UFF/Ensino/Disciplinas/Probabilidade-e-estatistica/Bookdown/dfimg/*
+rm -f /home/fnaufel/Documents/UFF/Ensino/Disciplinas/Probabilidade-e-estatistica/Bookdown/dfimg/*
 
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::bs4_book')"
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
