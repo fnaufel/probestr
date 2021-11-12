7b0429e108b13dd8a89af1823dac609e99aeed55
Fernando Naufel <fnaufel@gmail.com>
2021-11-12 12:02:37 -0300

    Tweak ch 3 and looks

--- _build.sh
+++ _build.sh
@@ -6,7 +6,7 @@ rm -f /home/fnaufel/Documents/UFF/Ensino/Disciplinas/Probabilidade-e-estatistica
 
 rm -f /home/fnaufel/Documents/UFF/Ensino/Disciplinas/Probabilidade-e-estatistica/Bookdown/dfimg/*
 
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::bs4_book')"
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
+r -v -e "bookdown::render_book('index.Rmd', 'bookdown::bs4_book')"
+r -v -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
 
 mv -f docs/_main.pdf docs/probestr.pdf
