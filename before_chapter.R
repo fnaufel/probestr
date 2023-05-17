1e2cf4002573724007bb7ea293dea23a2a786baa
Fernando Naufel <fnaufel@gmail.com>
2023-05-17 16:01:05 -0300

    Cap. sobre discretas. Mudei geometria do pdf.

--- before_chapter.R
+++ before_chapter.R
@@ -6,8 +6,11 @@ source(
 )
 
 library(patchwork)
+library(scales)
 
 # For interactive exercises
 # See https://github.com/fnaufel/exercises
 library(exercises)
 exercises::iniciar()
+
+set.seed(19000)
